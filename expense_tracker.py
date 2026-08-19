d1b0436684995b56315b14abf35e5338ef5259b1
Himanshu Shrestha <himanshushrestha000@gmail.com>
2026-08-19 19:07:08 +0545

    Add expense update and deletion

--- expense_tracker.py
+++ expense_tracker.py
@@ -10,10 +10,7 @@ def add_expense():
     "category": category
     }
     expenses.append(expense)
-
-    
-
-
+    print("Added Successfully!")
 
 def view_expenses():
     count=1
@@ -21,15 +18,44 @@ def view_expenses():
         print(f"{count}.{expense['description']} - Rs.{expense['amount']} - {expense['category']} ")
         count+=1
 
+def update_expense():
+    view_expenses()
+    update_choice=int(input("Choose an expense to update: "))
+
+    index=update_choice-1
+
+    new_description=input("Enter new description: ")
+    new_amount=int(input("Enter new amount: "))
+    new_category=input("Enter new category: ")
+    
+    selected_expense=expenses[index]
+
+    selected_expense['description']=new_description
+    selected_expense['amount']=new_amount
+    selected_expense['category']=new_category
+    print("Updated Successfully!")
+    
+
+def delete_expense():
+    view_expenses()
+    delete_choice=int(input("Choose an expense to delete: "))
+    
+    index=delete_choice-1
+
+    expenses.pop(index)
+    print("Deleted Successfully!")
+
 
 
 while True:
   print("""
-  Expense Tracker 
+Expense Tracker 
 
   1. Add Expense
   2. View Expenses
-  3. Exit
+  3. Update Expense
+  4. Delete Expense
+  5. Exit
 """)
   user_input=input("Choose an option: ")
   if user_input=="1":
@@ -37,6 +63,10 @@ while True:
   elif user_input=="2":
       view_expenses()
   elif user_input=="3":
+      update_expense()
+  elif user_input=="4":
+      delete_expense()
+  elif user_input=="5":
       break
   else:
       print("Invalid option")
